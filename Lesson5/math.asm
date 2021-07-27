7f61830c77c26a5839f51a466bbbd684322cece4
Matt Heffernan <slithymatt@hotmail.com>
2021-07-27 01:47:54 -0400

    started example, print_afbc working

--- Lesson5/math.asm
+++ Lesson5/math.asm
@@ -2,32 +2,54 @@
 
    org $8000
 
+   jp start
+
+sp_cache:
+   dw 0
+
 start:
-   ld c,$00
-   push bc
-   pop af
-   ld hl,$1234
-   ld de,$5678
-   add hl,de ; HL = $68AC
-   ld de,$8000
-   sbc hl,de ; HL = $E8AC + carry (overflow)
-   ccf
-   sbc hl,hl
+   ld a,$12
+   ld bc,$3456
+   call print_afbc
    ret
 
-printv:
+print_afbc:
    push af
-   push bc
-   jp pe,vset
-   ld a,$30
-   jp printv_doit
-vset:
-   ld a,$31
-printv_doit:
-   rst $10
-   pop bc
+   call print_hex
+   ld (sp_cache),sp
+   ld hl,(sp_cache)
+   ld a,(hl)
+   call print_hex
+   ld a,b
+   call print_hex
+   ld a,c
+   call print_hex
    pop af
    ret
+
+print_hex:
+   push af
+   srl a
+   srl a
+   srl a
+   srl a
+   call print_hex_digit
+   pop af
+   and $0F
+   call print_hex_digit
+   ret
+
+print_hex_digit:
+   cp $0A
+   jp p,print_letter
+   or $30
+   jp print_char
+print_letter:
+   add $37
+print_char:
+   rst $10
+   ret
+
 
 ; Deployment: Snapshot
    SAVESNA "math.sna", start
