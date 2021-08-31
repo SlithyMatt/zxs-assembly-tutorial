18dbc6dd99f76090a3dc68380b22a578345cec2a
Matt Heffernan <slithymatt@hotmail.com>
2021-08-31 01:05:16 -0400

    stack example working

--- Lesson6/stack.asm
+++ Lesson6/stack.asm
@@ -18,29 +18,38 @@ start:
    ;                   dec sp; ex (sp),hl; ex (sp),ix; ex (sp),iy
    ;                   ld sp,hl; ld sp,ix; ld sp,iy; sbc hl,sp; ld (xx),sp
    ;                   adc hl,sp; ld sp,(xx)
+   exx
+   push hl              ; preserve HL'
 
-   ; make giant checkerboard
+   ; make stripes
    ld ix,SCREEN_BITMAP  ; start at beginning of screen memory
    ld hl,$FF00          ; 16-pixel pattern
    ld bc,BITMAP_SIZE/2  ; whole screen
    call fill_ram        ; fill in pixel data
    ld ix,SCREEN_COLOR   ; start at beginning of color attribute memory
-   ld hl,$4747          ; white on black for 2 squares
+   ld hl,$2A2A          ; red on cyan for 2 squares
    ld bc,COLOR_SIZE/2   ; whole screen
    call fill_ram        ; fill in color attributes
+
+   pop hl
+   exx                  ; restore HL' to gracefully return to BASIC
    ret
 
 fill_ram:   ; IX = RAM address
             ; BC = number of words
             ; HL = word to write
    ld (sp_backup),sp    ; back up the stack pointer
+   add ix,bc
+   add ix,bc            ; IX = address after target segment
    ld sp,ix             ; make the RAM the stack
+   ld a,$FF
 .loop:
    push hl              ; write pattern
    dec c                ; decrement loop index
    jp nz,.loop
    dec b                ; lower byte wrapped around, decrement upper byte
-   jp nz,bitmap_loop
+   cp b                 ; check for underflow
+   jp nz,.loop
    ld sp,(sp_backup)    ; restore stack pointer
    ret
 
