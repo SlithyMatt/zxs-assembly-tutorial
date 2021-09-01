df55faaf4cc2a917104496c6538bdc80d82f2616
Matt Heffernan <matthew.heffernan.3@us.af.mil>
2021-09-01 15:44:24 -0400

    first draft of lesson 6 script

--- Lesson6/stack.asm
+++ Lesson6/stack.asm
@@ -17,13 +17,13 @@ start:
    push hl              ; preserve HL'
 
    ; make stripes
-   ld ix,SCREEN_BITMAP  ; start at beginning of screen memory
-   ld hl,$FF00          ; 16-pixel pattern
+   ld hl,SCREEN_BITMAP  ; start at beginning of screen memory
+   ld de,$FF00          ; 16-pixel pattern
    ld bc,BITMAP_SIZE/2  ; whole screen
    call fill_ram        ; fill in pixel data
 
-   ld ix,SCREEN_COLOR   ; start at beginning of color attribute memory
-   ld hl,$2A2A          ; red on cyan for 2 squares
+   ld hl,SCREEN_COLOR   ; start at beginning of color attribute memory
+   ld de,$2A2A          ; red on cyan for 2 squares
    ld bc,COLOR_SIZE/2   ; whole screen
    call fill_ram        ; fill in color attributes
 
@@ -31,16 +31,16 @@ start:
    exx                  ; restore HL' to gracefully return to BASIC
    ret
 
-fill_ram:   ; IX = RAM address
+fill_ram:   ; HL = RAM address
             ; BC = number of words
-            ; HL = word to write
+            ; DE = word to write
    ld (sp_backup),sp    ; back up the stack pointer
-   add ix,bc
-   add ix,bc            ; IX = address after target segment
-   ld sp,ix             ; make the RAM the stack
+   add hl,bc
+   add hl,bc            ; HL = address after target segment
+   ld sp,hl             ; make the RAM the stack
    ld a,$FF
 .loop:
-   push hl              ; write pattern
+   push de              ; write pattern
    dec c                ; decrement loop index
    jp nz,.loop
    dec b                ; lower byte wrapped around, decrement upper byte
