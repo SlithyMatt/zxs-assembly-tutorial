beda0b53367d67d27cbf0369b46956a2de7e4c0e
Matt Heffernan <matthew.heffernan.3@us.af.mil>
2021-11-30 15:14:07 -0500

    cleaned up IO code

--- Lesson8/io.asm
+++ Lesson8/io.asm
@@ -2,41 +2,24 @@
 
    org $8000
 
-   jp start
-
-   ; ROM routines
-ROM_CLS     = $0DAF
-ROM_PRINT   = $203C
-
 start:
-   exx
-   push hl              ; preserve HL'
-
-   ; demo instructions: in, out, ind, indr, ini, inir, outd, otdr, outi, otir
-
-   ld bc,$FEFE
-   ld d,0
+   ld bc,$FEFE          ; BC = Port $FEFE (in: keyboard[Shift,Z,X,C,V]; out: beeper)
+   ld d,0               ; D = byte to output (bit 4 toggled for beeper frequency)
 .keyloop:
-   in a,(c)
-   bit 2,a
-   jp nz,.soundoff
+   in a,(c)             ; A = key states
+   bit 2,a              ; Check bit 2 (X key)
+   jp nz,.soundoff      ; If bit is set, key is not pressed, turn off sound
 .soundon:
-   ld a,d
-   xor $10
-   ld d,a
+   ld a,d               ; A = last output byte
+   xor $10              ; flip bit 4
+   ld d,a               ; save new byte back to D
    jp .output
 .soundoff:
-   ld a,0
+   ld a,0               ; hold output to zero
 .output:
-   out (c),a
-   .500 nop
-   jp .keyloop
-
-   pop hl
-   exx               ; restore HL' to gracefully return to BASIC
-   ret
-
-
+   out (c),a            ; output determined byte value
+   .500 nop             ; do a busy wait (2000 T-states = 0.57 ms)
+   jp .keyloop          ; loop indefinitely
 
 ; Deployment: Snapshot
    SAVESNA "io.sna", start
