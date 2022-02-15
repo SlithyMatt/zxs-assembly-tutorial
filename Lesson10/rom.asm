2994d61a94626e6c6b48a6194bfbf26d400933e3
Matt Heffernan <slithymatt@hotmail.com>
2022-02-15 11:43:50 -0500

    more work on typewriter program

--- Lesson10/rom.asm
+++ Lesson10/rom.asm
@@ -3,17 +3,50 @@
    org $8000
 
 ; Routines
-PRINTOUT = $09A1
-KEYIN    = $10A8
+PRINTOUT    = $09F4
+KEYIN       = $10A8
+
+; ROM Data
+KEYTABLE_D  = $0260
 
 ; Control Codes
-INK      = $10
-
+ENTER       = $0D
+INK         = $10
 
 start:
+   im 1
+   ei
+key_loop:
    call KEYIN
-   jp nc,start
+   jp nc,key_loop
+   cp $20
+   jp m,check_control
+   cp $80
+   jp m,print
+check_control:
+   cp ENTER
+   jp z,print
+   ld hl,KEYTABLE_D
+   ld b,0
+   ld c,8
+ink_loop:
+   cp (hl)
+   jp z,do_ink
+   inc hl
+   inc b
+   dec c
+   jp nz,ink_loop
+   jp key_loop
+do_ink:
+   ld a,INK
+   push bc
    call PRINTOUT
+   pop bc
+   ld a,b
+   add a,$30
+print:
+   call PRINTOUT
+   jp key_loop
 
 ; Deployment: Snapshot
-   SAVESNA "rest.sna", start
+   SAVESNA "rom.sna", start
