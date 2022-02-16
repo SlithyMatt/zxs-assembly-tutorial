d440d789b1f078638f42ff0fb955dca1cf7b47b0
Matt Heffernan <slithymatt@hotmail.com>
2022-02-16 00:14:57 -0500

    typewriter working

--- Lesson10/rom.asm
+++ Lesson10/rom.asm
@@ -3,48 +3,51 @@
    org $8000
 
 ; Routines
-PRINTOUT    = $09F4
-KEYIN       = $10A8
+CL_ALL      = $0DAF
+KEY_INPUT   = $10A8
 
 ; Character Codes
 SPACE       = $20
-POUND       = $60
 COPYRIGHT   = $7F
 
 ; Control Codes
 ENTER       = $0D
 INK         = $10
+AT          = $AC
 
 start:
-   im 1
-   ei
+   im 1                 ; Use ROM-based interrupt routine
+   ei                   ; Enable maskable interrupts
+   call CL_ALL          ; Clear screen, reset cursor to top
 key_loop:
-   call KEYIN
-   jp nc,key_loop
-   cp POUND
-   jp z,do_ink
-   cp SPACE
-   jp m,check_enter
-   cp COPYRIGHT+1
-   jp m,print
-check_enter:
+   call KEY_INPUT       ; Get last key pressed
+   jp nc,key_loop       ; If C is clear, keep waiting for key press
+   cp AT                ; Key has been pressed
+   jp z,do_ink          ; If AT code (Symbol Shift + I), wait for ink color
    cp ENTER
-   jp z,print
-   jp key_loop
+   jp z,print           ; If ENTER code, print
+   cp SPACE
+   jp m,key_loop        ; If code < space character, ignore
+   cp COPYRIGHT+1
+   jp m,print           ; If code <= copyright character, print
+   jp key_loop          ; Else, ignore
 do_ink:
    ld a,INK
-   call PRINTOUT
+   rst $10              ; "print" INK code
 ink_loop:
-   call KEYIN
-   jp nc,ink_loop
+   call KEY_INPUT
+   jp nc,ink_loop       ; wait for another key press
    cp $30
-   jp m,key_loop
-   cp $3A
-   jp p,key_loop
-   sub $30
+   jp m,reset_ink       ; if code < '0', reset ink color
+   cp $38
+   jp p,reset_ink       ; if code >= '8', reset ink color
+   sub $30              ; get numerical value of numeral character...
+   jp print             ; ...and "print" it as ink value
+reset_ink:
+   ld a,0               ; reset ink to black by "printing" zero
 print:
-   call PRINTOUT
-   jp key_loop
+   rst $10              ; print final code
+   jp key_loop          ; keep looping indefinitely
 
 ; Deployment: Snapshot
    SAVESNA "rom.sna", start
